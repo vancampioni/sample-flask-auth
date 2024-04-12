2adf6677718bc3bbd24fd92597560def6a036c4f
Vanessa Campioni <vanessacrtsilva@gmail.com>
2024-04-12 08:22:33 -0300

    create user route

--- app.py
+++ app.py
@@ -42,6 +42,20 @@ def logout():
   logout_user()
   return jsonify({"message": "Logout realizado com sucesso!"})
 
+@app.route('/user', methods=["POST"])
+def create_user():
+  data = request.json
+  username = data.get("username")
+  password = data.get("password")
+
+  if username and password:
+    user = User(username=username, password=password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({"message": "Usuário cadastrado com sucesso"})
+
+  return jsonify({"message": "Dados inválidos"}), 400
+
 @app.route("/hello-world", methods=["GET"])
 def hello_world():
   return "Hello world"
